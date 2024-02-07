1a285d951342b9c0c8b68849092f4b10ade6f3e7
bargest <DIZz92@mail.ru>
2024-02-07 18:10:25 +0300

    add CBV

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from dogs.apps import DogsConfig
-from dogs.views import index, breeds, breeds_dogs
+from dogs.views import IndexView, BreedListView, DogListView, DogCreateView, DogUpdateView, DogDeleteView, BreedDetailView
 
 app_name = DogsConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('breeds/', breeds, name='breeds'),
-    path('<int:pk>/dogs/', breeds_dogs, name='breeds_dogs'),
+    path('', IndexView.as_view(), name='index'),
+    path('breeds/', BreedListView.as_view(), name='breeds'),
+    path('<int:pk>/dogs/', DogListView.as_view(), name='breeds_dogs'),
+    path('breed/detail/<int:pk>', BreedDetailView.as_view(), name='breed_detail'),
+    path('dogs/create', DogCreateView.as_view(), name='dog_create'),
+    path('dogs/update/<int:pk>', DogUpdateView.as_view(), name='dogs_update'),
+    path('dogs/delete/<int:pk>', DogDeleteView.as_view(), name='dogs_delete'),
 
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,32 +1,92 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView, DetailView
 
 from dogs.models import Breed, Dog
 
 
-def index(request):
+class IndexView(TemplateView):
+    template_name = 'dogs/index.html'
+    extra_context = {
+        'title': 'Питомник - Главная'
+    }
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['object_list'] = Breed.objects.all()[:3]
+        return context_data
+
+
+"""def index(request):
     context = {
         'object_list': Breed.objects.all()[:3],
         'title': 'Питомник - Главная'
     }
-    return render(request, 'dogs/index.html', context)
+    return render(request, 'dogs/index.html', context)"""
 
-
-def breeds(request):
+"""def breeds(request):
     context = {
         'object_list': Breed.objects.all(),
         'title': 'Питомник - Все наши породы'
     }
-    return render(request, 'dogs/breeds.html', context)
+    return render(request, 'dogs/breed_list.html', context)
+"""
 
 
-def breeds_dogs(request, pk):
+class BreedListView(ListView):
+    model = Breed
+    extra_context = {
+        'title': 'Питомник - Все наши породы'
+    }
+
+
+"""def breeds_dogs(request, pk):
     breed_item = Breed.objects.get(pk=pk)
     context = {
         'object_list': Dog.objects.filter(breed_id=pk),
+        'breed_pk': 'breed_item.pk',
         'title': f'Собаки породы - Все наши породы {breed_item.name}'
     }
-    return render(request, 'dogs/dogs.html', context)
+    return render(request, 'dogs/dog_list.html', context)"""
 
+
+class DogListView(ListView):
+    model = Dog
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(breed_id=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        breed_item = Breed.objects.get(pk=self.kwargs.get('pk'))
+        context_data['breed_pk'] = 'breed_item.pk',
+        context_data['title'] = f'Собаки породы - Все наши породы {breed_item.name}'
+
+        return context_data
+
+
+class BreedDetailView(DetailView):
+    model = Breed
+
+
+class DogCreateView(CreateView):
+    model = Dog
+    fields = ('name', 'breed',)
+    success_url = reverse_lazy('dogs:breeds')
+
+
+class DogUpdateView(UpdateView):
+    model = Dog
+    fields = ('name', 'breed',)
+    success_url = reverse_lazy('dogs:breeds')
+
+
+class DogDeleteView(DeleteView):
+    model = Dog
+    success_url = reverse_lazy('dog:breeds')
 
 
 """def index(request):
