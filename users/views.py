d8713337cc764e4299442c0da5163ecddf47b259
bargest <DIZz92@mail.ru>
2024-02-15 16:22:43 +0300

    add email_register_message

--- users/views.py
+++ users/views.py
@@ -1,7 +1,13 @@
+from django.conf import settings
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.core.mail import send_mail
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
+from users.forms import UserForm
+from users.models import User
 
 
 class LoginView(BaseLoginView):
@@ -10,3 +16,20 @@ class LoginView(BaseLoginView):
 
 class LogoutView(BaseLogoutView):
     pass
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/register.html'
+
+    def form_valid(self, form):
+        new_user = form.save()
+        send_mail(
+            subject='Поздравляем с регистрацией',
+            message='Вы зарегались на нашей платформе!',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[new_user.email]
+        )
+        return super().form_valid(form)
